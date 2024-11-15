25c9e5c075b9ccf8a52f4719a8f2fc3723883e61
Igo <igo.vieira@live.com>
2024-11-15 14:33:15 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,201 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import logo from "../../public/r12.png";
+import iconLixo from "../../public/iconLixo.png";
+import iconEditar from "../../public/iconEditar.png";
+import { useState } from "react";
+import generatePDF, { Margin } from "react-to-pdf";
+
+const personalizacao = {
+  method: "save",
+  page: {
+    // margin is in MM, default is Margin.NONE = 0
+    margin: Margin.MEDIUM,
+    // default is 'A4'
+    format: "A4",
+    // default is 'portrait'
+    orientation: "portrait",
+  },
+};
+
+const recuperarConteudoPDF = () => document.getElementById("conteudo");
 
 export default function Home() {
+  const [isPDFMode, setIsPDFMode] = useState(false);
+  const [name, setName] = useState("");
+  const [numero, setNumero] = useState("");
+  const [selectSize, setSelectSize] = useState("");
+  const [gender, setGender] = useState("");
+  const [uniformSet, setUniformSet] = useState("");
+  const [players, setPlayers] = useState([]);
+
+  const handleSavePlayer = () => {
+    if (name && numero && selectSize) {
+      const newPlayer = { name, numero, selectSize, gender, uniformSet };
+      setPlayers((prevPlayers) => [...prevPlayers, newPlayer]);
+      setName("");
+      setNumero("");
+      setSelectSize("");
+      setGender("");
+      setUniformSet("");
+    } else {
+      alert("Preencha todos os campos!");
+    }
+  };
+
+  const handleDeletePlayer = (index) => {
+    setPlayers((prevPlayers) => prevPlayers.filter((_, i) => i !== index));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="container mx-auto px-4 flex flex-col justify-center">
+      <div className="flex justify-center mt-5">
+        <Image alt="Logo" width={300} src={logo} />
+      </div>
+      <section className="mt-10 flex flex-wrap items-end  gap-3">
+        <div>
+          <p className="font-bold">Nome:</p>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Nome do jogador"
+            className="p-2 border-2 rounded-md border-gray-300"
+          />
         </div>
-      </div>
+        <div>
+          <p className="font-bold">Número:</p>
+          <input
+            value={numero}
+            onChange={(e) => setNumero(e.target.value)}
+            type="text"
+            placeholder="Número da camisa"
+            className="p-2 border-2 rounded-md border-gray-300"
+          />
+        </div>
+        <div>
+          <p className="font-bold">Tamanho:</p>
+          <select
+            value={selectSize}
+            onChange={(e) => setSelectSize(e.target.value)}
+            className="p-2 border-2 rounded-md border-gray-300"
+          >
+            <option value=""></option>
+            <option value="P">P</option>
+            <option value="M">M</option>
+            <option value="G">G</option>
+            <option value="GG">GG</option>
+            <option value="G1">G1</option>
+            <option value="G2">G2</option>
+          </select>
+        </div>
+        <div>
+          <p className="font-bold">Camiseta:</p>
+          <select
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+            className="p-2 border-2 rounded-md border-gray-300"
+          >
+            <option value=""></option>
+            <option value="Masculina">Masculina</option>
+            <option value="Feminina">Feminina</option>
+            <option value="Infantil">Infantil</option>
+          </select>
+        </div>
+        <div>
+          <p className="font-bold">Conjunto:</p>
+          <select
+            value={uniformSet}
+            onChange={(e) => setUniformSet(e.target.value)}
+            className="p-2 border-2 rounded-md border-gray-300"
+          >
+            <option value=""></option>
+            <option value="Sim">Sim</option>
+            <option value="Nao">Não</option>
+          </select>
+        </div>
+      </section>
+      <section className="flex gap-3 mt-5">
+        <div>
+          <button
+            onClick={handleSavePlayer}
+            className=" px-5 bg-blue-700 p-2 rounded-md text-white cursor-pointer"
+          >
+            Salvar jogador
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setIsPDFMode(true);
+              setTimeout(() => {
+                generatePDF(recuperarConteudoPDF, personalizacao).then(() => {
+                  setIsPDFMode(false); // Retorna ao modo scroll
+                });
+              }, 500); // Tempo para re-renderizar a lista completa
+            }}
+            className="px-5 bg-green-700 p-2 rounded-md text-white cursor-pointer"
+          >
+            Gerar PDF
+          </button>
+        </div>
+      </section>
+      <section id="conteudo" className="mt-10">
+        <h3 className="font-bold text-xl">Lista de jogadores</h3>
+        <p>Total: {players.length}</p>
+        <div className={`border-2 mt-2 rounded-md ${!isPDFMode ? "overflow-y-auto" : ""}`} style={!isPDFMode ? { maxHeight: "300px" } : {}}>
+          {players.length === 0 ? (
+            <p>Nenhum jogador salvo.</p>
+          ) : (
+            <ul>
+              {players
+                .slice() // Faz uma cópia da lista para evitar modificar o estado original
+                .sort((a, b) => {
+                  const sizeOrder = ["P", "M", "G", "GG", "G1", "G2"];
+                  return (
+                    sizeOrder.indexOf(a.selectSize) -
+                    sizeOrder.indexOf(b.selectSize)
+                  );
+                })
+                .map((player, index) => (
+                  <li
+                    key={index}
+                    className="text-lg flex items-center border-b py-2 justify-between px-2"
+                  >
+                    <span className="flex gap-2 flex-wrap">
+                      <span className="font-bold">Nome:</span> {player.name},
+                      <span className="font-bold">Número:</span> {player.numero}
+                      ,<span className="font-bold">Tamanho:</span>{" "}
+                      {player.selectSize},
+                      <span className="font-bold">Camiseta:</span>{" "}
+                      {player.gender},
+                      <span className="font-bold">Conjunto:</span>
+                      {player.uniformSet}
+                    </span>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                    {!isPDFMode && (
+                      <div className="flex flex-wrap gap-3 ml-5 justify-center items-center">
+                      <Image
+                        alt="Excluir jogador"
+                        src={iconLixo}
+                        className="cursor-pointer w-7"
+                        onClick={() => handleDeletePlayer(index)}
+                      />
+                      <Image
+                        alt="Editar jogador"
+                        src={iconEditar}
+                        className="cursor-pointer w-7"
+                        onClick={() => handleEditarPlayer(index)}
+                      />
+                    </div>
+                    )}
+                  </li>
+                ))}
+            </ul>
+          )}
+        </div>
+      </section>
     </main>
-  )
+  );
 }
