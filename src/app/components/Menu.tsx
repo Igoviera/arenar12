6a3c9003ad1dc54ec8d0b78bd25b36b48f170b39
Igo <igo.vieira@live.com>
2025-05-10 18:50:28 -0300

    correção de logo

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
         <header className="bg-black bg-opacity-15">
             <nav className="text-white italic flex justify-end gap-8 px-5 py-2">
                 <Link className="text-lg sm:text-xl md:text-xl hover:text-gray-300" href={"/"}>Inicio</Link>
-                {/*<Link className="text-lg sm:text-xl md:text-xl hover:text-gray-300" href={"/tabelaMedidas"}> Tabela de medidas </Link>*/}
+                <Link className="text-lg sm:text-xl md:text-xl hover:text-gray-300" href={"/tabelaMedidas"}> Tabela de medidas </Link>
             </nav>
         </header>
     )
